9f890285aa0a2c298374ac20575057fdebd4ba39
bongiss <39141183+bongiss@users.noreply.github.com>
2021-06-28 21:40:43 +1200

    wip

--- page-objects/Main Page/MainPage.ts
+++ page-objects/Main Page/MainPage.ts
@@ -11,30 +11,87 @@ export class MainPage extends BasePage{
     PopularModel_img = 'img.img-fluid.center-block';
     OverallRating_img = 'img.img-fluid.center-block';
     ModelList_img = 'img.img-thumbnail';
-    Comment_textArea = 'textarea[id="comment"]'
+    Comment_textArea = 'textarea[id="comment"]';
+    Vote_button = 'button.btn.btn-success';
+    VoteCount_label = 'h4 strong';
+    ThankYou_label = 'p.card-text';
+    Engine_label = 'li strong';
+    OverallRating_table = 'table.cars.table.table-hover';
+    AuthorFirstRow_table = 'tr td:nth-child(2)';
+    RankingFirstRow_table = 'tr td:nth-child(4)';
+    RankHeader_table = 'tr th:nth-child(4)'; 
+
+
     
     async goto_homepage(){
         await this.page.click(this.BuggyRating_nav_logo);
     }
 
-    async clickPopularMake(){
+    async clickPopularMakeSection(){
         await this.page.waitForSelector(':nth-match('+ this.PopularMake_img + ',1)');
         await this.page.click(':nth-match('+ this.PopularMake_img + ',1)');
     }
 
-    async clickCarModel(){
+    async clickPopularModelSection(){
+        await this.page.waitForSelector(':nth-match('+ this.PopularMake_img + ',2)');
+        await this.page.click(':nth-match('+ this.PopularMake_img + ',2)');
+    }
+
+    async clickOverallRatingSection(){
+        await this.page.waitForSelector(':nth-match('+ this.PopularMake_img + ',3)');
+        await this.page.click(':nth-match('+ this.PopularMake_img + ',3)');
+    }
+    
+    async selectCarModel(){
         await this.page.waitForSelector(this.ModelList_img);
         const all = await this.page.$$(this.ModelList_img);
-        Math.floor(Math.random()*(all-1+1)+1);
-        await this.page.click(':nth-match('+ this.ModelList_img + ',1)');
+        const randomElement = Math.floor(Math.random() * (all.length - 1 + 1)) + 1;
+        await this.page.click(':nth-match('+ this.ModelList_img + ','+ randomElement +')');
     }
 
-    async enterComment(){
+    async verifyCarEngine(){
+        await this.page.waitForSelector(this.Engine_label);
+        return this.page.isVisible(':nth-match('+ this.Engine_label + ',1)');
+    }
+
+    async verifyMaxSpeed(){
+        await this.page.waitForSelector(this.Engine_label);
+        return this.page.isVisible(':nth-match('+ this.Engine_label + ',2)');
+    }
+
+    async enterComment(text:string){
+        const votecount = await this.page.innerText(this.VoteCount_label);
+        const nextvotecount = parseInt(votecount)+ 1;
         await this.page.waitForSelector(this.Comment_textArea);
-        await this.page.fill(this.Comment_textArea, 'beautiful car');
+        await this.page.fill(this.Comment_textArea, 'vote count from ' + votecount +' to ' + nextvotecount+ ' via ' + text);
     }
 
+    async voteandverify(){
+        const votecount = await this.page.innerText(this.VoteCount_label);
+        await this.page.click(this.Vote_button);
+        await this.page.waitForSelector(this.ThankYou_label);
+        return votecount < this.page.innerText(this.VoteCount_label);
+    }
 
+    async verifyOverAllRatingTable(){
+        await this.page.waitForSelector(this.OverallRating_table);
+        return this.page.isVisible(this.OverallRating_table);
+    }
+
+    async verifyAuthorColumn(){
+        await this.page.waitForSelector(this.AuthorFirstRow_table);
+        return await this.page.innerText(this.AuthorFirstRow_table);
+    }
+
+    async sortbyRank(){
+        await this.page.waitForSelector(this.RankHeader_table);
+        await this.page.click(this.RankHeader_table);
+    }
+
+    async verifyRankColumn(){
+        await this.page.waitForSelector(this.RankingFirstRow_table);
+        return await this.page.innerText(this.RankingFirstRow_table);
+    }
 
 
 }
--- tests/TechAssessment/TechAssementTest.spec.ts
+++ tests/TechAssessment/TechAssementTest.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { makeString } from "../../utilities/randomStringGenerator";
 import { MainPage } from "../../page-objects/Main Page/MainPage";
-import { ProductsPage } from "../../page-objects/Products Page/ProductsPage";
 import { RegisterPage } from "../../page-objects/Register Page/RegisterPage";
 import BasePage  from "../../page-objects/BasePage";
 
@@ -43,15 +42,71 @@ test.describe("Buggy Cars E2E:", () => {
 
     });
 
-    test.only("Popular Vote", async ({page}) => {
+    test("View Popular Car Model and verify Details", async ({page}) => {
+        await mainPage.clickPopularModelSection();
+        expect(await mainPage.verifyCarEngine()).toBe(true);
+        expect(await mainPage.verifyMaxSpeed()).toBe(true);
+    });
+
+    test("View Popular Make and view Random Car Model and Details", async ({page}) => {
+        await mainPage.clickPopularMakeSection();
+        await mainPage.selectCarModel();
+        expect(await mainPage.verifyCarEngine()).toBe(true);
+        expect(await mainPage.verifyMaxSpeed()).toBe(true);
+    });
+
+
+    test("Vote via Popular Make", async ({page}) => {
         let username = makeString(8);
         await registerPage.register(username,'Morgan','Evangelio','Qwerty.101');
+        await registerPage.verifyRegistrationSuccessful();
         await basePage.login(username,'Qwerty.101');
         await mainPage.goto_homepage();
-        await mainPage.clickPopularMake();
-        await mainPage.clickCarModel();
-        await mainPage.enterComment();
-
+        await mainPage.clickPopularMakeSection();
+        await mainPage.selectCarModel();
+        await mainPage.enterComment("Popular Make Section");
+        expect(await mainPage.voteandverify()).toBe(true);
     });
+
+    test("Vote via popular model", async ({page}) => {
+        let username = makeString(8);
+        await registerPage.register(username,'Morgan','Evangelio','Qwerty.101');
+        await registerPage.verifyRegistrationSuccessful();
+        await basePage.login(username,'Qwerty.101');
+        await mainPage.goto_homepage();
+        await mainPage.clickPopularModelSection();
+        await mainPage.enterComment("Popular Model Section");
+        expect(await mainPage.voteandverify()).toBe(true);
+    });
+
+    test("View Overall Car Rating", async ({page}) => {
+        await mainPage.clickOverallRatingSection();
+        expect(await mainPage.verifyOverAllRatingTable()).toBe(true);
+    });
+
+    test("View and vote a random car in Overall Car Rating Section", async ({page}) => {
+        let username = makeString(8);
+        await registerPage.register(username,'Morgan','Evangelio','Qwerty.101');
+        await registerPage.verifyRegistrationSuccessful();
+        await basePage.login(username,'Qwerty.101');
+        await mainPage.goto_homepage();
+        await mainPage.clickOverallRatingSection();
+        await mainPage.selectCarModel();
+        await mainPage.enterComment("Overall Rating Section");
+        expect(await mainPage.voteandverify()).toBe(true);
+    });
+
+    test("Verify comment table", async ({page}) => {
+        await mainPage.clickPopularMakeSection();
+        await mainPage.selectCarModel();
+        expect(await mainPage.verifyAuthorColumn()).not.toEqual('');
+    });
+
+    test("Sort by rank", async ({page}) => {
+        await mainPage.clickOverallRatingSection();
+        await mainPage.sortbyRank();
+        expect(await mainPage.verifyRankColumn()).toBe("10"); 
+    });
+        
 
 });
