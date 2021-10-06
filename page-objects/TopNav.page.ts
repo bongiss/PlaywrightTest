22f07ca5598126dbeda8c8906c03e4f84703aab7
bongiss <39141183+bongiss@users.noreply.github.com>
2021-10-07 10:21:15 +1300

    WIP

---
+++ page-objects/TopNav.page.ts
@@ -0,0 +1,5 @@
+import { Page } from '@playwright/test';
+
+export class TopBarNav {
+    public _contact = this._select('tn') 
+}
--- tests/TechAssessment/TechAssementTest.spec.ts
+++ tests/TechAssessment/TechAssementTest.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from "@playwright/test";
 import { UrlActions } from "../../test-helpers/UrlAction.page";
 
-test.describe('', () => {
-    test('', async ({ page }) => {
+test.describe('Technical Assessment Automation', () => {
+    test('Test Case 1', async ({ page }) => {
         const urlActions = new UrlActions(page);
         
         await urlActions.goToUrl('http://jupiter.cloud.planittesting.com/');
+        
     });
 });
