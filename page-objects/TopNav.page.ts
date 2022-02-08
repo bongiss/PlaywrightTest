317552238ef03e462acd083994d8e474e975f021
bongiss <39141183+bongiss@users.noreply.github.com>
2022-02-09 10:36:40 +1300

    update helper=package imports

--- page-objects/TopNav.page.ts
+++ page-objects/TopNav.page.ts
@@ -1,5 +1,5 @@
 import { Page } from '@playwright/test';
-import { createSelector, Selector } from '../Helper-package/src';
+import { createSelector, Selector } from '../helper-package/src';
 
 export class TopNav {
     private _contact = this._select('a[href="#/contact"]');
